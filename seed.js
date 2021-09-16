62e68fd23ea4b400c9b284ea5bb035e077d5b9a4
fagnermeds <fagnermeds@gmail.com>
2021-09-16 19:39:08 -0300

    feat: add feature to create admin user in seed.js file

--- seed.js
+++ seed.js
@@ -1,1 +1,21 @@
-// colocar query do MongoDB
+const { MongoClient } = require('mongodb');
+
+const mongoDbUrl = 'mongodb://localhost:27017/Cookmaster';
+
+(async () => {
+  connection = await MongoClient.connect(mongoDbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = connection.db('Cookmaster');
+
+  await db.collection('users').insertOne({
+    name: 'admin',
+    email: 'root@email.com',
+    password: '123456',
+    role: 'admin',
+  });
+
+  process.exit();
+})();
